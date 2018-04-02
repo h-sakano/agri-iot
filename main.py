a84e0d0c7b967ac0237d3f3f00be3c7671d355fb
h-sakano <h.sakano@world-bit.co.jp>
2018-04-02 22:46:06 +0900

    Line通知を追加

--- main.py
+++ main.py
@@ -4,24 +4,48 @@ import dht11
 import yl69
 import time
 import datetime
+from linebot import LineBotApi
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 # initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
+# initialize line messaging api
+line_bot_api = LineBotApi('Your Access Token')
+
+# initialize others
+lastThirsty = datetime.datetime(1970, 1, 1)
+lastWatering = datetime.datetime(1970, 1, 1)
+
 # read data using pin 11
 dht11 = dht11.DHT11(pin=11)
 yl69 = yl69.YL69(pinCLK=2, pinDIO=4, pinCS=3, pinYl69Vcc=5)
 
 while True:
-    dht11Result = dht11.read()
-    if dht11Result.is_valid():
-        print("Last valid input: " + str(datetime.datetime.now()))
-        print("Temperature: %d C" % dht11Result.temperature)
-        print("Humidity: %d %%" % dht11Result.humidity)
+    now = datetime.datetime.now()
 
-    yl69Result = yl69.getResult(0)
-    print("Soil moisture: %d" % yl69Result)
+    if now.minute == 0 and now.second == 0:
+        dht11Result = dht11.read()
+        if dht11Result.is_valid():
+            print("Last valid input: " + str(datetime.datetime.now()))
+            print("Temperature: %d C" % dht11Result.temperature)
+            print("Humidity: %d %%" % dht11Result.humidity)
 
-    time.sleep(1)
+    if now.minute % 10 == 0 and now.second == 0:
+        yl69Result = yl69.getResult(0)
+        print("Soil moisture: %d" % yl69Result)
+
+        if yl69Result > 180:
+            delta = now - lastThirsty
+            if delta.seconds > 1800:
+                line_bot_api.push_message("Your User ID", TextSendMessage(text='もう喉からから・・・。誰か水ちょうだいm(__)m'))
+                lastThirsty = datetime.datetime.now()
+        if yl69Result < 70:
+            if lastWatering < lastThirsty:  # 最後に水やりに喜んだ時間が、最後に喉乾いたメッセージを送信するよりも前の場合
+                line_bot_api.push_message("Your User ID", TextSendMessage(text='誰かが水をくれたよ！ありがとう！'))
+                lastWatering = datetime.datetime.now()
